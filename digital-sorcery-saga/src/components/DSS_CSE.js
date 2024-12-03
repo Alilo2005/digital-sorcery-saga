5d75e42e784f9966a57457e7ec7ddc6afbb1c5bd
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-03 04:31:58 +0000

    DSS_CSE

---
+++ digital-sorcery-saga/src/components/DSS_CSE.js
@@ -0,0 +1,9 @@
+import './DSS_CSE.css'
+export default function DSS_CSE(){
+    return (
+        <div className="logos">
+            <img src="badge.png" alt="DSS Logo" width="200"></img>
+            <img src="cse.png" alt="CSE Logo" width="200"></img>
+        </div>
+    );
+}
--- digital-sorcery-saga/src/components/Home.js
+++ digital-sorcery-saga/src/components/Home.js
@@ -2,12 +2,14 @@ import './Home.css';
 import LogosRegister from './LogosRegister';
 import Navbar from './Navbar'
 import Title from './Title';
+import DSS_CSE from './DSS_CSE';
 function Home() {
   return (
     <div className="Home">
       <LogosRegister />
       <Navbar />
       <Title />
+      <DSS_CSE />
     </div>
   );
 }
