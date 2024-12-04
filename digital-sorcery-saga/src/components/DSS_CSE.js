f59966c24f9e972cde9fd6b65cc98f12e15fc6cd
Alilo2005 <ma_mahdi@esi.dz>
2024-12-04 01:44:23 +0000

    video player

--- digital-sorcery-saga/src/components/DSS_CSE.js
+++ digital-sorcery-saga/src/components/DSS_CSE.js
@@ -1,9 +1,14 @@
 import './DSS_CSE.css'
+import Header from './Header';
 export default function DSS_CSE(){
     return (
-        <div className="logos">
-            <img src="badge.png" alt="DSS Logo" width="100"></img>
-            <img src="cse.png" alt="CSE Logo" width="100"></img>
-        </div>
+         <>
+            <div className="logos">
+               <Header />
+               <img src="badge.png" alt="DSS Logo" width="100"></img>
+               <img src="cse.png" alt="CSE Logo" width="100"></img>
+            </div>
+         </>
+
     );
 }
