d27a91071ad019cf9b9971af17e2c72881ee8b63
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-04 17:26:20 +0000

    minor chages

--- digital-sorcery-saga/src/App.js
+++ digital-sorcery-saga/src/App.js
@@ -5,12 +5,14 @@ import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Workshops from './components/Workshops/Workshops';
 import Contact from './components/Contacts/Contact';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <>
         <Router>
           <Navbar />
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path='/Workshops' element={<Workshops />} />
