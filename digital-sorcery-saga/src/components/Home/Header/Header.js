2679e76ed2186e00cf37ff5ede5c921ffd2e32e9
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-04 16:43:39 +0000

    song to play lets rule

--- digital-sorcery-saga/src/components/Home/Header/Header.js
+++ digital-sorcery-saga/src/components/Home/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header className="header">
             <div className="video-section">
                 <video controls className="video-player">
-                    <source src="path-to-video.mp4" type="video/mp4" />
+                    <source src="./Neoni x burnboy - Champion (Official Lyric Video)(M4A_128K).m4a" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
