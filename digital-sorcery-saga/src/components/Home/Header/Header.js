fe1f5b653ae8b16f976723e68a0d487e03e6c028
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-04 21:35:52 +0000

    final commit

--- digital-sorcery-saga/src/components/Home/Header/Header.js
+++ digital-sorcery-saga/src/components/Home/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header className="header">
             <div className="video-section">
                 <video controls className="video-player">
-                    <source src="./Neoni x burnboy - Champion (Official Lyric Video)(M4A_128K).m4a" type="video/mp4" />
+                    <source src="#" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
