faed08a9f56351c740f18461a0fb0fe906ad9d6b
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-04 13:36:46 +0000

    routes configured

--- digital-sorcery-saga/src/App.js
+++ digital-sorcery-saga/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import LogosRegister from './components/LogosRegister';
+import React from 'react';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-        <Home />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          
+        </Router>
     </>
   );
 }
--- digital-sorcery-saga/src/components/Navbar.js
+++ digital-sorcery-saga/src/components/Navbar.js
@@ -1,18 +1,29 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
     return (
+        <div className='nav'>
+         <div className="logos-register">
+          <div className="logo-container">
+            <img src="tech.png" alt="Tech Logo" className="tech"></img> 
+            <img src="wisdom.png" alt="Wisdom Logo" className="wisdom"></img> 
+            <img src="creativity.png" alt=" Creativity Logo" className="creativity"></img> 
+          </div>
+          <button className="register-btn">REGISTER</button>
+        </div>
         <nav className="navbar">
             <ul className="nav-links">
-                <li>Home</li>
-                <li>About</li>
-                <li>Agenda</li>
-                <li>Workshops</li>
-                <li>Sponsors</li>
-                <li>FAQ</li>
-                <li>Contacts</li>
+            <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/agenda">Agenda</Link></li>
+              <li><Link to="/workshops">Workshops</Link></li>
+              <li><Link to="/sponsors">Sponsors</Link></li>
+              <li><Link to="/faq">FAQ</Link></li>
+              <li><Link to="/contacts">Contacts</Link></li>
             </ul>
         </nav>
+    </div>
     );
 }
 
