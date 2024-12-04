a97c7a6165cd719adc23fc08c7a56c89f8c43841
Alilo2005 <ma_mahdi@esi.dz>
2024-12-04 01:18:25 +0000

    idk

--- digital-sorcery-saga/src/components/DSS_CSE.js
+++ digital-sorcery-saga/src/components/DSS_CSE.js
@@ -2,8 +2,8 @@ import './DSS_CSE.css'
 export default function DSS_CSE(){
     return (
         <div className="logos">
-            <img src="badge.png" alt="DSS Logo" width="200"></img>
-            <img src="cse.png" alt="CSE Logo" width="200"></img>
+            <img src="badge.png" alt="DSS Logo" width="100"></img>
+            <img src="cse.png" alt="CSE Logo" width="100"></img>
         </div>
     );
 }
