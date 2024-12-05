38b3879fb5790a7008881564cfed24c3c93cbb1c
MAHDI ALI <ma_mahdi@esi.dz>
2024-12-05 11:14:31 +0000

    About page integrated

--- digital-sorcery-saga/src/App.js
+++ digital-sorcery-saga/src/App.js
@@ -5,13 +5,14 @@ import Home from './components/Home/Home';
 import Workshops from './components/Workshops/Workshops';
 
 import Contact from './components/Contacts/Contact'
+import About from './components/About/About';
 
 function App() {
     return (
         <div>
             <Navbar />
             <div id="home"><Home /></div>
-
+            <div id="about"><About /></div>
             <div id="workshops"><Workshops /></div>
  
             <div id="contacts"><Contact /></div>
---
+++ digital-sorcery-saga/src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './About.css';
+const About = () => {
+    return (
+        
+      <div className="about">
+        <h2>About the event</h2>
+        <div className='description'>
+          <div class="drop-cap"><h6>T</h6></div>
+          <p className='first'>
+            The Digital Sorcery Saga DSS, inspired by the Harry Potter film series, is an immersive event dedicated
+            to graphic design, web development, and UIUX design that lasts for two weeks. Open to all enthusiasts of
+            these fields, this event offers them the opportunity to work on stimulating projects and meet other professionals to enrich their network.
+            <br />
+            The themes are focused on current and relevant issues in the digital world, allowing participants
+            to confront the real world challenges of their disciplines
+          </p>
+        </div>
+        <button className="button">learn more</button>
+      </div>
+      
+
+   
+    )
+}
+
+export default About;
